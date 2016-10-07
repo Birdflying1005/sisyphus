883ea9acd6c65cf76ecf6fe5abf27620ab356644
thereisnospoon <vindimon@gmail.com>
2016-10-07 17:54:40 +0300

    Added support for streaming without possibility of rewinding

--- streaming/src/main/scala/my/thereisnospoon/sisyphus/streaming/GridFsClient.scala
+++ streaming/src/main/scala/my/thereisnospoon/sisyphus/streaming/GridFsClient.scala
@@ -1,21 +1,43 @@
 package my.thereisnospoon.sisyphus.streaming
 
 import com.mongodb.MongoClient
+import com.mongodb.async.SingleResultCallback
+import com.mongodb.async.client.MongoClients
 import com.mongodb.client.FindIterable
 import com.mongodb.client.model.Sorts._
 import com.mongodb.client.model.Filters
+import com.mongodb.client.model.Projections._
 import org.bson.Document
 import org.bson.types.ObjectId
+
+import scala.concurrent.{Future, Promise}
 
 object GridFsClient {
 
   val mongoUri = "mongodb://localhost:27017"
   val dbName = "webm-haus"
 
+  private val asyncMongoClient = MongoClients.create(mongoUri)
   private val mongoClient = new MongoClient
-  private val db = mongoClient.getDatabase(dbName)
-  private val chunksCollections = db.getCollection("fs.chunks")
+  private val chunksCollection = mongoClient.getDatabase(dbName).getCollection("fs.chunks")
+  private val filesCollection = asyncMongoClient.getDatabase(dbName).getCollection("fs.files")
 
   def retrieveChunksForFile(fileId: String): FindIterable[Document] =
-    chunksCollections.find(Filters.eq("files_id", new ObjectId(fileId))).sort(ascending("n"))
+    chunksCollection.find(Filters.eq("files_id", new ObjectId(fileId))).sort(ascending("n"))
+
+  def retrieveFileSize(fileId: String): Future[Long] = {
+
+    val lengthRetrievePromise = Promise[Long]
+
+    filesCollection.find(Filters.eq("_id", new ObjectId(fileId))).projection(include("length"))
+      .first((res: Document, t: Throwable) => {
+
+        Option(res) match {
+          case Some(document) => lengthRetrievePromise.success(document.getLong("length"))
+          case _ => lengthRetrievePromise.failure(t)
+        }
+      })
+
+    lengthRetrievePromise.future
+  }
 }
--- streaming/src/main/scala/my/thereisnospoon/sisyphus/streaming/HttpEndpoint.scala
+++ streaming/src/main/scala/my/thereisnospoon/sisyphus/streaming/HttpEndpoint.scala
@@ -1,31 +1,46 @@
 package my.thereisnospoon.sisyphus.streaming
 
-import akka.NotUsed
 import akka.actor.ActorSystem
 import akka.http.scaladsl.Http
-import akka.stream.ActorMaterializer
 import akka.http.scaladsl.model._
+import akka.stream.ActorMaterializer
 import akka.http.scaladsl.server.Directives._
 import akka.stream.scaladsl.Source
-import akka.http.scaladsl.marshalling.PredefinedToEntityMarshallers._
-import akka.http.scaladsl.marshalling.PredefinedToResponseMarshallers._
+import akka.http.scaladsl.model.MediaType.NotCompressible
+import akka.http.scaladsl.model.headers._
+import akka.http.scaladsl.model.headers.RangeUnits.Bytes
 import akka.util.ByteString
+
+import scala.util.{Failure, Success}
 
 object HttpEndpoint extends App {
 
   implicit val system = ActorSystem("streaming-system")
   implicit val materializer = ActorMaterializer()
 
+  def extractRange: PartialFunction[HttpHeader, ByteRange] = {
+    case Range(Bytes, range :: _) => range
+  }
+
   val route =
     path(Slash.?) {
       get {
-        complete(HttpEntity(ContentTypes.`text/html(UTF-8)`, "<h1>Streaming endpoint</h1>"))
+        complete(HttpResponse(entity = HttpEntity(ContentTypes.`text/html(UTF-8)`, "<h1>Streaming endpoint</h1>")))
       }
     } ~
-    pathPrefix("video" / HexLongNumber) {fileId =>
+    path("video" / """\w+""".r) {fileId =>
       get {
-        val fileDataSource: Source[ByteString, NotUsed] = Source.fromGraph(new GridFsSource(fileId.toHexString))
-        complete(fileDataSource)
+
+        onComplete(GridFsClient.retrieveFileSize(fileId)) {
+          case Failure(ex) => complete(HttpResponse(status = StatusCodes.NotFound))
+
+          case Success(fileLength) => {
+            val fileDataSource: Source[ByteString, _] = Source.fromGraph(new GridFsSource(fileId))
+            complete(HttpResponse(headers = List(`Accept-Ranges`(RangeUnits.Bytes)),
+              entity = HttpEntity(MediaType.video("webm", NotCompressible, "webm"), fileLength, fileDataSource)))
+          }
+        }
+
       }
     }
 
